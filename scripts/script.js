6bc02b8df20db1820ae09f8b9793bc58e80188a8
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-02-11 11:51:49 +0200

    backing up..

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,25 @@
 // Learning jQuery
 // November 7, 2018
 
-const element = '<p>Page successfully loaded!</p>';
+jQuery(document).ready(() => {
+  // change the footer message at the bottom of the page
+  $('#message').append('Ready to Manipulate!');
 
-$('document').ready(() => {
-  $('#content').append(element);
+  // animate div width on hover
+  jQuery('#rect')
+    // animate width to 200px on mouseover
+    .on('mouseover', () => {
+      jQuery('#rect').animate({ width: 200 }, 300);
+    })
+
+    // animate width to 100px on mouseout
+    .on('mouseout', () => {
+      jQuery('#rect').animate({ width: 100 }, 300);
+    })
+
+    // increase height by 100px on click
+    .on('click', () => {
+      jQuery('#rect').animate({ height: '+=100' }, 'slow');
+    });
 });
+// --- end of code
