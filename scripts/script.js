f3f9dfc6470fd7aee765870e6ae324d7b2875f76
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-03-18 18:42:02 +0200

    backing up..

--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,23 @@
 // November 7, 2018
 
 jQuery(document).ready(() => {
-  // change the footer message at the bottom of the page
-  $('#message').append('Ready to Manipulate!');
+  // document ready message
+  jQuery("footer small").text("Ready to Manipulate!");
 
-  // animate div width on hover
-  jQuery('#rect')
-    // animate width to 200px on mouseover
-    .on('mouseover', () => {
-      jQuery('#rect').animate({ width: 200 }, 300);
-    })
-
-    // animate width to 100px on mouseout
-    .on('mouseout', () => {
-      jQuery('#rect').animate({ width: 100 }, 300);
-    })
-
-    // increase height by 100px on click
-    .on('click', () => {
-      jQuery('#rect').animate({ height: '+=100' }, 'slow');
-    });
+  //   // jQuery('#rect')
+  //   //   // animate width to 200px on mouseover
+  //   //   .on('mouseover', () => {
+  //   //     jQuery('#rect').animate({ width: 200 }, 300);
+  //   //   })
+  //   //
+  //   //   // animate width to 100px on mouseout
+  //   //   .on('mouseout', () => {
+  //   //     jQuery('#rect').animate({ width: 100, height: 100 }, 300);
+  //   //   })
+  //   //
+  //   //   // increase height by 100px on click
+  //   //   .on('click', () => {
+  //   //     jQuery('#rect').animate({ height: '+=100' }, 'slow');
+  //   //   });
 });
-// --- end of code
+// --- end of code ----
