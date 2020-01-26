ee69b4ea8ec449bd0e1a99e48bd7ae62a5cfedfa
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2020-01-27 00:17:14 +0200

    backing up...

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,20 @@
 jQuery(document).ready(() => {
   // document ready message
   jQuery("footer small").text("Ready to Manipulate!");
+  jQuery("#rect")
+    // animate width to 200px on mouseover
+    .on("mouseover", () => {
+      jQuery("#rect").animate({ width: 200 }, 300);
+    })
 
-  //   // jQuery('#rect')
-  //   //   // animate width to 200px on mouseover
-  //   //   .on('mouseover', () => {
-  //   //     jQuery('#rect').animate({ width: 200 }, 300);
-  //   //   })
-  //   //
-  //   //   // animate width to 100px on mouseout
-  //   //   .on('mouseout', () => {
-  //   //     jQuery('#rect').animate({ width: 100, height: 100 }, 300);
-  //   //   })
-  //   //
-  //   //   // increase height by 100px on click
-  //   //   .on('click', () => {
-  //   //     jQuery('#rect').animate({ height: '+=100' }, 'slow');
-  //   //   });
+    // animate width to 100px on mouseout
+    .on("mouseout", () => {
+      jQuery("#rect").animate({ width: 100, height: 100 }, 300);
+    })
+
+    // increase height by 100px on click
+    .on("click", () => {
+      jQuery("#rect").animate({ height: "+=100" }, "slow");
+    });
 });
 // --- end of code ----
